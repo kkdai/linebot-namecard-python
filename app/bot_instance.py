75e0340654d2949c9653984ff568c9a0bcb4751d
Evan Lin <evan.if.lin@linecorp.com>
2026-05-26 08:46:37 +0800

    Phase 1 Fix: Lazily initialize ClientSession and AsyncLineBotApi to fix event loop crash on startup

--- app/bot_instance.py
+++ app/bot_instance.py
@@ -4,13 +4,31 @@ from linebot import AsyncLineBotApi, WebhookParser
 from linebot.aiohttp_async_http_client import AiohttpAsyncHttpClient
 from . import config
 
-session = aiohttp.ClientSession()
-async_http_client = AiohttpAsyncHttpClient(session)
-line_bot_api = AsyncLineBotApi(config.CHANNEL_ACCESS_TOKEN, async_http_client)
+
+class LazyLineBotApi:
+    def __init__(self):
+        self._api = None
+        self.session = None
+
+    def _get_api(self):
+        if self._api is None:
+            self.session = aiohttp.ClientSession()
+            async_http_client = AiohttpAsyncHttpClient(self.session)
+            self._api = AsyncLineBotApi(
+                config.CHANNEL_ACCESS_TOKEN, async_http_client
+            )
+        return self._api
+
+    def __getattr__(self, name):
+        return getattr(self._get_api(), name)
+
+
+line_bot_api = LazyLineBotApi()
 parser = WebhookParser(config.CHANNEL_SECRET)
 
 user_states = {}
 
 
 async def close_session():
-    await session.close()
+    if line_bot_api.session:
+        await line_bot_api.session.close()
